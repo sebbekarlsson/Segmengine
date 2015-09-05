800ddfe387b3ba4a315e4fc9d5b6e396359e653b
= <sebbekarlsson97@gmail.com>
2015-09-05 11:52:17 +0200

    Documentation for the Scene class

--- src/rt/main/Scene.java
+++ src/rt/main/Scene.java
@@ -7,20 +7,44 @@ import rt.main.actors.Entity;
 
 public abstract class Scene {
 	
+	/*
+	 * The scene obviously needs a camera...
+	 */
 	public Camera camera = new Camera(this, 0, 0, 0);
+	
+	/*
+	 * Creating a buffer that will be used to update & draw all of our objects (actors) .
+	 */
 	protected ArrayList<Actor> actors = new ArrayList<Actor>();
+	
+	/*
+	 * This variable is used later to do initializing.
+	 */
 	private boolean initialized = false;
+	
+	/*
+	 * Setting the default background color for a scene.
+	 */
 	public Color backgroundColor = Color.black;
 	
+	/**
+	 * Used to update the current state of the scene.
+	 * 
+	 * @param delta
+	 */
 	public void update(int delta){
+		/*
+		 * Updating the scene's logic & graphics.
+		 */
 		tick(delta);
 		draw(delta);
 		
+		/*
+		 * Updating and initializing all of the actors.
+		 */
 		for(int i = 0; i < actors.size(); i++){
 			actors.get(i).update(delta);
 			actors.get(i).getHitbox().update();
-			
-			
 			
 			if(!actors.get(i).isInitialized()){
 				actors.get(i).initialize();
@@ -28,39 +52,88 @@ public abstract class Scene {
 		}
 	}
 	
+	/**
+	 * Used to initialize the scene.
+	 */
 	public abstract void init();
+	
+	/**
+	 * Used to update the scene's logic.
+	 * 
+	 * @param delta
+	 */
 	public abstract void tick(int delta);
+	
+	/**
+	 * Used to draw the scene's graphics.
+	 * 
+	 * @param delta
+	 */
 	public abstract void draw(int delta);
 
+	/**
+	 * 
+	 * @return the camera for this scene.
+	 */
 	public Camera getCamera(){
 		return this.camera;
 	}
 	
+	/**
+	 * Adds an actor to our actor-buffer.
+	 * 
+	 * @param actor
+	 */
 	public void stageActor(Actor actor){
 		actors.add(actor);
 	}
 	
+	/**
+	 * Removes an actor from the scene's actor-buffer.
+	 * 
+	 * @param actor
+	 */
 	public void removeActor(Actor actor){
 		actors.remove(actor);
 	}
 	
+	/**
+	 * Removes an actor from the scene's actor-buffer where the index = index.
+	 * 
+	 * @param index
+	 */
 	public void removeActor(int index){
 		actors.remove(index);
 	}
 	
+	/**
+	 * 
+	 * @return a boolean value that represents the current initialization-state.
+	 */
 	public boolean isInitialized(){
 		return this.initialized;
 	}
 	
+	/**
+	 * Initializes and sets the current boolean state to initialized (true).
+	 */
 	public void initialize(){
 		init();
 		this.initialized = true;
 	}
 	
+	/**
+	 * 
+	 * @return a list with the actors of the scene. (actor-buffer)
+	 */
 	public ArrayList<Actor> getActors(){
 		return this.actors;
 	}
 	
+	/**
+	 * 
+	 * @return a list with the entities of the scene. (actor-buffer, but only entities)
+	 */
 	public ArrayList<Entity> getEntities(){
 		ArrayList<Entity> entities = new ArrayList<Entity>();
 		for(int i = 0; i < getActors().size(); i++){
