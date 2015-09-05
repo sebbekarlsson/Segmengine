65d95742257ff24bd609380ab038e6f53f41ba17
= <sebbekarlsson97@gmail.com>
2015-09-05 15:35:43 +0200

    Doc update

--- src/rt/main/utils/Smart.java
+++ src/rt/main/utils/Smart.java
@@ -1,8 +1,20 @@
 package rt.main.utils;
 
+/**
+ * This class is used for math.
+ * 
+ * @author Sebastian Robert Karlsson
+ */
 public class Smart {
+	
+	/**
+	 * This function is basically modulo but it will always return a positive result.
+	 * 
+	 * @param x the value we want to perform modulo on.
+	 * @param y the modulo value.
+	 * @return the positive product from x % y as an Integer. 
+	 */
 	public static int mod(float x, float y){
-
 		int r = (int)x % (int)y;
 		if (r < 0)
 		{
